3c558aa7fd1eb57775826b1bc0716791d946d32c
Semih-Sinani <semihsinaniofficial@gmail.com>
2023-08-16 18:52:46 +0200

    Added def for sppech rec

--- streamdeck.py
+++ streamdeck.py
@@ -3,3 +3,8 @@ import speech_recognition as sr #sesli kontrıl kütüphanesii
 import pyttsx3
 import webbrowser #chrome bağlan
 import datetime
+
+def recognize_speech_for_search():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
