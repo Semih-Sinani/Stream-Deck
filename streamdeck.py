56fb7bc1c0896faf7137128b4ac016392bbc07a6
Semih-Sinani <semihsinaniofficial@gmail.com>
2023-08-16 18:53:14 +0200

    Added for search in ggole

--- streamdeck.py
+++ streamdeck.py
@@ -8,3 +8,28 @@ def recognize_speech_for_search():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source)
+
+          try:
+        text = r.recognize_google(audio, language="tr-TR")
+        search_text_entry.delete("1.0", tk.END)
+        search_text_entry.insert(tk.END, text)
+        perform_search()
+    except sr.UnknownValueError:
+        search_text_entry.delete("1.0", tk.END)
+        search_text_entry.insert(tk.END, "Ses anlaşılamadı.")
+    except sr.RequestError as e:
+        search_text_entry.delete("1.0", tk.END)
+        search_text_entry.insert(tk.END, "Ses tanıma servisine erişilemedi; {0}".format(e))
+#hatalara sonuç ata
+def text_to_speech(lang):
+    text = search_text_entry.get("1.0", tk.END).strip()
+    if text:
+        engine = pyttsx3.init()
+        engine.setProperty("rate", 150)  # Ses hızını ayarlayabilirsin
+        engine.setProperty("volume", 0.8)  # Ses düzeyini ayarlayabilirsin
+        engine.setProperty("voice", lang)  # Seçilen dili ayarlar
+        engine.say(text)
+        engine.runAndWait()
+        search_text_entry.delete("1.0", tk.END)
+    else:
+        search_text_entry.delete("1.0", tk.END)
